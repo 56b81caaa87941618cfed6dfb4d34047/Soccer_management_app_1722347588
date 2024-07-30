0ae778c47bbe6c9cd595fae36454198cb0f05c98
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-30 13:53:46 +0000

    Merge code from frontend

--- components/simple_footer_component_1722347590.js
+++ components/simple_footer_component_1722347590.js
@@ -2,14 +2,14 @@
 */
 Vue.component("simple_footer_component_1722347590", {
     template: `
-    <footer id="footer-section" class="flex-1 bg-white dark:bg-gray-800">
+    <footer id="footer-section" class="flex-1 bg-red-600 dark:bg-gray-800">
             <div id="footer-container" class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
                 <hr id="footer-divider" class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
                 <div id="footer-content" class="text-center">
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            Soccer Mania: Your Ultimate Soccer Companion
+                            Soccer Mania: Your Soccer Companion
                         </a>
                     </div>
                 </div>
